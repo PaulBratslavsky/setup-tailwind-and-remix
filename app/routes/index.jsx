23c227cd25854569f7aaf00085975b01b9b452de
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-02-17 21:37:14 -0600

    How to set up Tailwind with Remix

---
+++ app/routes/index.jsx
@@ -0,0 +1,202 @@
+export default function Index() {
+  return (
+    <section className="py-20">
+      <div className="container px-4 mx-auto">
+        <div className="flex flex-wrap -mx-4 -mb-16">
+          <div className="w-full md:w-1/2 px-4 mb-16">
+            <div className="flex flex-wrap -mx-4">
+              <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                <div className="h-80 lg:h-40">
+                  <img
+                    className="w-full h-full object-cover rounded-xl"
+                    src="https://images.unsplash.com/photo-1544717305-996b815c338c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-2/3 px-4">
+                <span className="inline-block mb-4 text-xs text-gray-500">
+                  10 jun 2020 19:40
+                </span>
+                <h2 className="mb-4 text-3xl font-semibold font-heading">
+                  Lorem ipsum dolor sit
+                </h2>
+                <p className="mb-4 text-gray-500 leading-relaxed">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+                <a
+                  className="text-lg font-medium text-red-500 underline hover:no-underline"
+                  href="#"
+                >
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-4 mb-16">
+            <div className="flex flex-wrap -mx-4">
+              <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                <div className="h-80 lg:h-40">
+                  <img
+                    className="w-full h-full object-cover object-top rounded-xl"
+                    src="https://images.unsplash.com/photo-1544717305-2782549b5136?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-2/3 px-4">
+                <span className="inline-block mb-4 text-xs text-gray-500">
+                  10 jun 2020 19:40
+                </span>
+                <h2 className="mb-4 text-3xl font-semibold font-heading">
+                  Lorem ipsum dolor sit
+                </h2>
+                <p className="mb-4 text-gray-500 leading-relaxed">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+                <a
+                  className="text-lg font-medium text-red-500 underline hover:no-underline"
+                  href="#"
+                >
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-4 mb-16">
+            <div className="flex flex-wrap -mx-4">
+              <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                <div className="h-80 lg:h-40">
+                  <img
+                    className="w-full h-full object-cover rounded-xl"
+                    src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-2/3 px-4">
+                <span className="inline-block mb-4 text-xs text-gray-500">
+                  10 jun 2020 19:40
+                </span>
+                <h2 className="mb-4 text-3xl font-semibold font-heading">
+                  Lorem ipsum dolor sit
+                </h2>
+                <p className="mb-4 text-gray-500 leading-relaxed">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+                <a
+                  className="text-lg font-medium text-red-500 underline hover:no-underline"
+                  href="#"
+                >
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-4 mb-16">
+            <div className="flex flex-wrap -mx-4">
+              <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                <div className="h-80 lg:h-40">
+                  <img
+                    className="w-full h-full object-cover rounded-xl"
+                    src="https://images.unsplash.com/photo-1544717297-fa95b6ee9643?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-2/3 px-4">
+                <span className="inline-block mb-4 text-xs text-gray-500">
+                  10 jun 2020 19:40
+                </span>
+                <h2 className="mb-4 text-3xl font-semibold font-heading">
+                  Lorem ipsum dolor sit
+                </h2>
+                <p className="mb-4 text-gray-500 leading-relaxed">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+                <a
+                  className="text-lg font-medium text-red-500 underline hover:no-underline"
+                  href="#"
+                >
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-4 mb-16">
+            <div className="flex flex-wrap -mx-4">
+              <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                <div className="h-80 lg:h-40">
+                  <img
+                    className="w-full h-full object-cover object-top rounded-xl"
+                    src="https://images.unsplash.com/photo-1544717305-2782549b5136?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-2/3 px-4">
+                <span className="inline-block mb-4 text-xs text-gray-500">
+                  10 jun 2020 19:40
+                </span>
+                <h2 className="mb-4 text-3xl font-semibold font-heading">
+                  Lorem ipsum dolor sit
+                </h2>
+                <p className="mb-4 text-gray-500 leading-relaxed">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+                <a
+                  className="text-lg font-medium text-red-500 underline hover:no-underline"
+                  href="#"
+                >
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-4 mb-16">
+            <div className="flex flex-wrap -mx-4">
+              <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                <div className="h-80 lg:h-40">
+                  <img
+                    className="w-full h-full object-cover rounded-xl"
+                    src="https://images.unsplash.com/photo-1525129498994-580709352e20?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-2/3 px-4">
+                <span className="inline-block mb-4 text-xs text-gray-500">
+                  10 jun 2020 19:40
+                </span>
+                <h2 className="mb-4 text-3xl font-semibold font-heading">
+                  Lorem ipsum dolor sit
+                </h2>
+                <p className="mb-4 text-gray-500 leading-relaxed">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+                <a
+                  className="text-lg font-medium text-red-500 underline hover:no-underline"
+                  href="#"
+                >
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
